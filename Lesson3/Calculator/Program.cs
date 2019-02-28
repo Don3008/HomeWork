58e64d8aac0a1da404d40ec14a6c1ea69bfb3575
Don3008 <don3008@yandex.ru>
2019-02-28 15:11:51 +0300

    Дполнен калькулятор

--- Lesson3/Calculator/Program.cs
+++ Lesson3/Calculator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Calculator
 {
@@ -15,24 +16,32 @@ namespace Calculator
         Exponentiation,
         Root,
     }
+    enum ChoiseAct
+    {
+        ContinueCalculation = 1,
+        NewCalculation,
+        OutputValue,
+        Exit
+    }
     class Program
     {
         static double result;
         static void Main(string[] args)
         {
+            string path = @"C:\Users\don30\source\repos\HomeWork\Lesson3\Calculator\result.txt";
             DisplayMenu(new Operation());
             Operation operation = ChoiceFunction();
             double firstValue = InputFirstValue();
             double secondValue = InputSecondValue();
             double result = Calculation(firstValue, secondValue, operation);
-            NextAct(result);
+            ChoiseAct choiseAct = new ChoiseAct();
+            NextAct(result, path, choiseAct);
         }
         static void DisplayMenu(Operation operation)
         {
             Console.WriteLine("Выберете действие, которое хотите совершить над числами.");
-            Console.WriteLine($" 1.{Operation.Add} - Сложение \n 2.{Operation.Substruct} - Вычитание \n " +
-                $"3.{Operation.Multiply} - Умножение \n 4.{Operation.Divide} - Деление \n 5.{Operation.Exponentiation} - " +
-                $"Возведение в степень \n 6.{Operation.Root} - Извлечение корня");
+            Console.WriteLine($" {(int)Operation.Add} - Сложение \n {(int)Operation.Substruct} - Вычитание \n " +
+                $"{(int)Operation.Multiply} - Умножение \n {(int)Operation.Divide} - Деление \n {(int)Operation.Exponentiation} - " + $"Возведение в степень \n {(int)Operation.Root} - Извлечение корня");
         }
         static Operation ChoiceFunction()
         {
@@ -91,26 +100,57 @@ namespace Calculator
             Console.WriteLine($"Результат: {result}");
             return result;
         }
-        static void NextAct(double result)
+        static void Write(string path, double newValue)
+        {
+            using (FileStream stream = new FileStream(path, FileMode.Append))
+            { }
+            using (StreamWriter writer = new StreamWriter(path, true, Encoding.Default))
+            {
+                writer.WriteLine(newValue);
+            }
+        }
+        static void Read(string path, int position)
+        {
+            using (StreamReader reader = new StreamReader(path, Encoding.Default))
+            {
+                string line;
+                for (int i = 1; i < position; i++)
+                {
+                    reader.ReadLine();
+                }
+                line = reader.ReadLine();
+                Console.WriteLine("Переменная: " + line);
+            }
+        }
+        static double NewValue()
+        {
+            Console.Write("Введите значение: ");
+            double newValue = double.Parse(Console.ReadLine());
+            return newValue;
+        }
+        static void NextAct(double result, string path, ChoiseAct choiseAct)
         {
             bool isExit = true;
             while (isExit)
             {
-                Console.WriteLine("Выберете дальнейшее действие: \n 1. Выполнить действие с предыдущим результатом. \n" +
-                    "2. Начать новый расчет. \n 3. Выход.");
-                int op = int.Parse(Console.ReadLine());
-                switch (op)
+                Console.WriteLine($"Выберете дальнейшее действие: \n {(int)ChoiseAct.ContinueCalculation}. Выполнить действие с предыдущим результатом. \n" +
+                    $" {(int)ChoiseAct.NewCalculation}. Начать новый расчет. \n {(int)ChoiseAct.OutputValue}. Введите номер промежуточной переменной, которую хотите вывести на экран. \n {(int)ChoiseAct.Exit}. Выход.");
+                int input = int.Parse(Console.ReadLine());
+                choiseAct = (ChoiseAct)input;
+                switch (choiseAct)
                 {
-                    case 1:
+                    case ChoiseAct.ContinueCalculation:
                         Console.Clear();
                         DisplayMenu(new Operation());
                         Console.WriteLine($"Предыдущий результат: {result}");
                         Operation func = ChoiceFunction();
-                        Console.Write("Введите значение: ");
-                        double newValue = double.Parse(Console.ReadLine());
+                        double newValue = NewValue();
+                        Write(path, newValue);
+
                         result = Calculation(result, newValue, func);
                         break;
-                    case 2:
+                    case ChoiseAct.NewCalculation:
+                        Delete(path);
                         Console.Clear();
                         DisplayMenu(new Operation());
                         Operation operation = ChoiceFunction();
@@ -118,14 +158,24 @@ namespace Calculator
                         double secondValue = InputSecondValue();
                         Calculation(firstValue, secondValue, operation);
                         break;
-                    case 3:
+                    case ChoiseAct.OutputValue:
+                        Console.Write("Введите номер позиции переменной, которую хотите вывести: ");
+                        int position = int.Parse(Console.ReadLine());
+                        Read(path, position);
+                        break;
+                    case ChoiseAct.Exit:
                         isExit = false;
+                        Delete(path);
                         break;
                     default:
                         Console.WriteLine("Неверный ввод!");
                         break;
                 }
             }
+        }
+        static void Delete(string path)
+        {
+            File.Delete(path);
         }
     }
 }
