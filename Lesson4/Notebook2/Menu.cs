c324b55061f2f87a68609b204b187219d9c82e7d
Don3008 <don3008@yandex.ru>
2019-03-09 15:58:41 +0300

    Вторая версия "Заметок"

---
+++ Lesson4/Notebook2/Menu.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook2
+{
+    class Menu
+    {
+        public bool Repeat { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\don30\source\repos\HomeWork\Notebook2\";
+            bool isExit = false;
+            //int count = 0;
+            while (!isExit)
+            {
+                
+                Note note = new Note(path);
+                
+                string text = InputText();
+                isExit = NewNote(text, path, isExit, note);
+                
+            }
+            
+        }
+
+
+
+        public static bool NewNote(string text, string path, bool isExit, Note note)
+        {
+            ConsoleKeyInfo key = new ConsoleKeyInfo();
+            Console.WriteLine("Хотите создать еще одну заметку? \n Нажмите Y, если да, N - если нет");
+            key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Y)
+            {
+                
+                note.Write(text);
+            }
+            else if (key.Key == ConsoleKey.N)
+            {
+                isExit = true;
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод!");
+            }
+            return isExit;
+        }
+
+        public static string InputText()
+        {
+            Console.WriteLine("Введите информацию, которую хотите сохранить.");
+            string text = Console.ReadLine();
+            return text;
+        }
+    }
+
+}
+
---
+++ Lesson4/Notebook2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Notebook2
+{
+    class Note
+    {
+        public string Name { get; private set; }
+        public DateTime CreateTime { get; private set; }
+        public bool Exists { get; private set; }
+        public string Path { get; private set; }
+        public string Text { get; private set; }
+
+        private FileInfo FileNote;
+        private int count = 0;
+
+        public Note(string path)
+        {
+            Name = count.ToString();
+            Path = path + Name + ".txt";
+            FileNote = new FileInfo(Path);
+            Exists = FileNote.Exists;
+            CreateTime = DateTime.Now;
+            count++;
+            //Text = ReadFile();
+        }
+
+        private string ReadFile()
+        {
+            string text;
+            using (StreamReader reader = new StreamReader(Path, Encoding.Default))
+            {
+                text = reader.ReadToEnd();
+            }
+            return text;
+        }
+
+        public void Write(string text)
+        {
+            // Проверка
+            if (!Exists)
+            {
+                FileNote.Create();
+                CreateTime = DateTime.Now;
+            }
+            //Записали
+            string dateToday = CreateTime.ToString();
+            using (StreamWriter writer = new StreamWriter(Path, false, Encoding.Default))
+            {
+                writer.WriteLine(dateToday);
+                writer.WriteLine(text);
+            }
+            //Считали
+            Text = ReadFile();
+        }
+
+        public void Delete()
+        {
+            // Delete File
+        }
+    }
+
+
+
+
+    
+}
+
